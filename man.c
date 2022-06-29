01bc7ccfb3b459b561cef5835b51c03e95384062
ArkenZis <rahil.haneef10@gmail.com>
2022-06-29 14:15:52 +0400

    Added characterAge again

--- man.c
+++ man.c
@@ -8,7 +8,7 @@ int main()
 
     printf("There once was a man named %s\n", characterName);
     printf("he was %d years old.\n", characterAge);
-
+    // Changing the age midway story
     characterAge = 30;
     printf("He really liked the name %s\n", characterName);
     printf("but did not like being %d.\n", characterAge);
