09d0acc702b91b29e1ce0f750773bfcb0f695299
ArkenZis <rahil.haneef10@gmail.com>
2022-06-29 14:13:28 +0400

    Added Variables

--- man.c
+++ man.c
@@ -3,11 +3,13 @@
 int main()
 {
 
-    char characterName[] = "John";
+    char characterName[] = "Tom";
     int characterAge = 35;
 
-    printf("There once was a man named John\n");
-    printf("he was 35 years old.\n");
-    printf("He really liked the name John\n");
-    printf("but did not like being 35.\n");
+    printf("There once was a man named %s\n", characterName);
+    printf("he was %d years old.\n", characterAge);
+    printf("He really liked the name %s\n", characterName);
+    printf("but did not like being %d.\n", characterAge);
+
+    return 0;
 }
