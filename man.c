61e75bec86a0246c6a4c0d1f3cdf805d62b8e4f5
ArkenZis <rahil.haneef10@gmail.com>
2022-06-29 14:00:58 +0400

    Created man.c and typed down a story

---
+++ man.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char characterName[] = "John";
+    int characterAge = 35;
+
+    printf("There once was a man named John\n");
+    printf("he was 35 years old.\n");
+    printf("He really liked the name John\n");
+    printf("but did not like being 35.\n");
+}
