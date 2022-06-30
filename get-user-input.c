5e8a61f6ffc148f69037fdee320bc26f4f30f2e6
ArkenZis <rahil.haneef10@gmail.com>
2022-06-30 12:51:13 +0400

    Added 'Asking for age function and comments'

--- get-user-input.c
+++ get-user-input.c
@@ -3,12 +3,21 @@
 
 int main()
 {
+    // Asking for GPA
     double gpa;
 
     printf("Enter your gpa: \n");
     scanf("%lf", &gpa);
 
     printf("Your gpa is: %f\n", gpa);
+
+    // Asking for age
+    int age;
+
+    printf("Enter your age: \n");
+    scanf("%d", &age);
+
+    printf("You are %d years old\n", age);
 
     return 0;
 }
