75475a52ab49e9442c021e86f40ab8d512013c9c
ArkenZis <rahil.haneef10@gmail.com>
2022-06-30 13:25:56 +0400

    Added variables and edited output

--- mad-libs-game.c
+++ mad-libs-game.c
@@ -3,9 +3,14 @@
 
 int main()
 {
-    printf("Roses are red\n");
-    printf("Violets are blue\n");
-    printf("I love you\n");
+
+    char color[20];
+    char pluralNoun[20];
+    char celebrity[20];
+
+    printf("Roses are {color}\n");
+    printf("{plural-noun} are blue\n");
+    printf("I love {celebrity}\n");
 
     return 0;
 }
