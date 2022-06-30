44154d51f4b922343273c81d7c613a8811e10d4f
ArkenZis <rahil.haneef10@gmail.com>
2022-06-30 13:19:18 +0400

    Basic calculator done

--- basic-calculator.c
+++ basic-calculator.c
@@ -4,4 +4,17 @@
 int main()
 {
     // This is the start of the Basic Calculator Program
+
+    int num1;
+    int num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    printf("Answer: %d\n", num1 + num2);
+
+    return 0;
 }
