3622ea1c0ba1dfc45730c85e5ec73ca28d6e19f3
ArkenZis <rahil.haneef10@gmail.com>
2022-07-01 20:00:33 +0400

    Corrected comment

--- arrays.c
+++ arrays.c
@@ -7,7 +7,7 @@ int main()
 
     int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
 
-    // Printing out a the 0th position element of array
+    // Printing out a the 2nd position element of array
     printf("%d\n", luckyNumbers[2]);
     return 0;
 }
