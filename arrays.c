4777d2a62f9c136a2c8ee7cf42854aac34f3bb41
ArkenZis <rahil.haneef10@gmail.com>
2022-07-01 19:58:41 +0400

    Progress

--- arrays.c
+++ arrays.c
@@ -3,6 +3,11 @@
 
 int main()
 {
+    // Creating an array
 
+    int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
+
+    // Printing out a the 0th position element of array
+    printf("%d\n", luckyNumbers[0]);
     return 0;
 }
