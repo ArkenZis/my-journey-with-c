3406df2c390ebd2bd6e882789c646d8875248860
ArkenZis <rahil.haneef10@gmail.com>
2022-07-01 20:02:47 +0400

    Changed the 1st position element value and printed

--- arrays.c
+++ arrays.c
@@ -9,5 +9,10 @@ int main()
 
     // Printing out a the 2nd position element of array
     printf("%d\n", luckyNumbers[2]);
+
+    // Changing value of 1st position element of array
+    luckyNumbers[1] = 200;
+    printf("%d\n", luckyNumbers[1]);
+
     return 0;
 }
